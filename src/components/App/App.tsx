c60e81a806adcc7e5e1fee5e57dd066da7a88c1c
Yevhenii Shevchenko <s.yevhenii.shevchenko@gmail.com>
2026-03-25 11:40:26 +0200

    Initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { notify, toastOptions } from '../../helpers/hot-toast';
+
+import css from './App.module.css';
+import type { Movie } from '../../types/movie';
+import { fetchMovies } from '../../services/movieService';
+
+import SearchBar from '../SearchBar/SearchBar';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import MovieModal from '../MovieModal/MovieModal';
+
+function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const handleSearch = async (query: string) => {
+    setMovies([]);
+    setIsLoading(true);
+    setIsError(false);
+    try {
+      const movies = await fetchMovies(query);
+      if (!movies.length) {
+        notify('No movies found for your request.');
+        return;
+      }
+
+      setMovies(movies);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+    document.body.style.overflow = 'hidden';
+    return;
+  };
+
+  const handleSelect = (movie: Movie) => {
+    setSelectedMovie(movie);
+    openModal();
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedMovie(null);
+    document.body.style.overflow = '';
+    return;
+  };
+
+  return (
+    <div className={css.app}>
+      <SearchBar onSubmit={handleSearch} />
+      <Toaster toastOptions={toastOptions} />
+      {isError ? (
+        <ErrorMessage />
+      ) : (
+        <MovieGrid onSelect={handleSelect} movies={movies} />
+      )}
+      {isLoading && <Loader />}
+      {isModalOpen && <MovieModal movie={selectedMovie} onClose={closeModal} />}
+    </div>
+  );
+}
+
+export default App;
