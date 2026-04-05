ba7cbb5f74315d5c8c2b179348f7726b3b72f390
S.Yevhenii.Serhiiovych <s.yevhenii.shevchenko@gmail.com>
2026-04-05 16:28:36 +0300

    Install React Query Devtools and ADD

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './components/App/App.tsx';
 
 import 'modern-normalize';
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 );
